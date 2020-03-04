6d55a11c973e71386d7350c9ea5ef8557b7b411f
Jandy4 <justandygames@gmail.com>
2020-03-04 23:47:49 +0200

    restructured files and directories using fbs

--- App/target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'Biblioteca', 'author': 'Andy', 'version': '0.0.0', 'environment': 'local'}
