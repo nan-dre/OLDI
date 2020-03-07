8c97962aaca064ee2726ce6f5f7818066190edc1
Jandy4 <justandygames@gmail.com>
2020-03-07 22:29:49 +0200

    Access resources via AppContext

--- src/main/python/mainmenu.py
+++ src/main/python/mainmenu.py
@@ -1,20 +1,22 @@
 from PyQt5 import QtWidgets, uic
+from fbs_runtime.application_context.PyQt5 import ApplicationContext
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 
 class MainWindow(QtWidgets.QMainWindow):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, ui, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         #Load the UI Page
-        uic.loadUi('UIs\\mainmenu.ui', self)
-
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
-    sys.exit(app.exec_())
+        uic.loadUi(ui, self)
 
 if __name__ == '__main__':      
-    main()
+    
+    appctxt = ApplicationContext()
+    ui = appctxt.get_resource('UIs\mainmenu.ui')
+    
+    main = MainWindow(ui)
+    main.show()
+    
+    exit_code = appctxt.app.exec_()
+    sys.exit(exit_code)
